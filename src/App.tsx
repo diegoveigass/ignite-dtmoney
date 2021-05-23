68100bfbcf5509b7bc4fdaa314cefe4af58a39ad
diegoveigass <sdiegoveiga@gmail.com>
2021-05-22 22:05:39 -0300

    Instalando Styled Components
    Criado o primeiro componente styled na aplicação, Title. Instalação do styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: blueviolet;
+`;
+
 export function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <Title>Hello World</Title>
     </div>
   );
 }
