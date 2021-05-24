1a5eca0b9bc28b36e894ee8ff2d19e970ae9244d
diegoveigass <sdiegoveiga@gmail.com>
2021-05-23 21:12:14 -0300

    Componente: TransactionsTable
    Criação do nosso componente TransactionsTable e aplicação da estilização

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from './styles';
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>28/05/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$600</td>
+            <td>Casa</td>
+            <td>15/05/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
