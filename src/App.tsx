3f45747693ccce30b6f7890a3c6f412c6424cbef
diegoveigass <sdiegoveiga@gmail.com>
2021-05-26 20:54:53 -0300

    Componente: NewTransactionModal
    Criação de um novo componente que irá contemplar a nossa modal de transação, que é onde iremos adicionar nosso HTML na pagina futuramente. Mantido o estado e as funções de abrir e fechar a modal no nosso App.tsx visando o compartilhamento das funções relacionadas a modal entre outros componentes.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
 
@@ -19,17 +20,16 @@ export function App() {
   function handleCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false);
   }
+
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
