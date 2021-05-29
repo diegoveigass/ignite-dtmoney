34becc2be490e5a1c16f01008e3c794a95d4e0d4
diegoveigass <sdiegoveiga@gmail.com>
2021-05-29 18:00:55 -0300

    Criando e listando transações
    Utilizado função seed do miragejs para adicionarmos alguns dados pré-cadastrados na nossa rota. Criado rota de post para poder criar as transações. Criado estado para armazenar as transações que recebemos da API

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,11 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 
 import { Container } from './styles';
 
+interface Transaction {
+  id: number;
+  title: string;
+  type: 'deposit' | 'withdraw';
+  category: string;
+  value: number;
+  createdAt: string;
+}
+
 export function TransactionTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get('/transactions').then(response => console.log(response.data));
+    api
+      .get('/transactions')
+      .then(response => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -21,18 +34,14 @@ export function TransactionTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>28/05/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$600</td>
-            <td>Casa</td>
-            <td>15/05/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>R${transaction.value}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
